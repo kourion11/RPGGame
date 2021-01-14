3ce836df5e5b10d0ae71de5ac9fabc27316714a5
user <user@WIN-B6EAV0SJMD6>
2021-01-14 17:43:40 +0300

    1.2

--- src/main.kt
+++ src/main.kt
@@ -9,35 +9,64 @@ fun main(args: Array<String>) {
     var raceChar: String = ""
     var hp: Int = 0
     var dmg: Int = 0
+    var ageHero: Int = 0
+    var ageHeroText: String = ""
 
+    //Корректность возраста
     if(age in 1..100)
-        age = age
+        age
     else
         println("Введите корректное значение!")
 
-    if(race in 1..3) {
-        if(race == 1) {
-            raceChar = "Человек"
-            hp = 10
-            dmg = 10 }
-        else if(race == 2) {
-            raceChar = "Орк"
-            hp = 12
-            dmg = 8 }
-        else if(race == 3) {
-            raceChar = "Эльф"
-            hp = 8
-            dmg = 12 }
-        else
-            println("Выберите рассу из списка!") }
+    //Проверка возраста
+    when(age) {
+        in 1..30 -> ageHero = 1
+        in 30..60 -> ageHero = 2
+        in 60..100 -> ageHero = 3
+    }
+
+    //Расса героя
+    if(race == 1) {
+        raceChar = "Человек"
+        hp = 10
+        dmg = 10 }
+    else if(race == 2) {
+        raceChar = "Орк"
+        hp = 12
+        dmg = 8 }
+    else if(race == 3) {
+        raceChar = "Эльф"
+        hp = 8
+        dmg = 12 }
     else
         println("Выберите вариант от 1 до 3")
 
+    //Возраст героя
+    if(ageHero == 1) {
+        ageHeroText = "Ваш персонаж молод, Вы получаете +2 к хп и +2 к урону"
+        hp += 2
+        dmg += 2
+    } else if(ageHero == 2) {
+        ageHeroText = "Ваш персонаж средних лет"
+    } else if(ageHero == 3) {
+        ageHeroText = "Ваш персонаж стар, Вы теряете -2 хп и -2 от урона"
+        hp -= 2
+        dmg -= 2
+    } else
+            println("Не получилось определить возраст")
+
+    //Вывод характеристик персонажа
+    char(name, age, ageHeroText, raceChar, hp, dmg)
+
+}
+
+fun char(name: String?, age: Int, ageHeroText: String, raceChar: String, hp: Int, dmg: Int) {
     println("")
+    println("Персонаж успешно создан!")
     println("Ваш никнейм: $name")
     println("Ваш возраст: $age")
+    println(ageHeroText)
     println("Ваша расса: $raceChar")
     println("Кол-во здоровья: $hp")
     println("Кол-во урона: $dmg")
-
 }
